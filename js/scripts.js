1c46e3bf43409a3fc1e2c58df12fed5a0bf9540d
NIER_BIAN <58038514+CoolGuySergey@users.noreply.github.com>
2024-05-10 22:02:10 +0800

    initialise objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
-alert('Hello world');
-
-let favoriteFood = 'potato';
-document.write(favoriteFood);
+let songList =[
+    {
+	name: 'Blank Space',
+	album: '1989',
+	year: 2023,
+	category: ['happy', 'breakup'],
+    },
+    {
+	name: 'You Need To Calm Down',
+	album: 'Lover',
+	year: 2019,
+	category: ['happy']
+    },
+    {
+	name: 'Death By A Thousand Cuts',
+	album: 'Lover',
+	year: 2019,
+	category: ['sad', 'breakup'],
+    }
+];
