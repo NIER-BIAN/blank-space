dcf2b73ba2b440c4b187bef4d3a01498b32d316e
NIER_BIAN <58038514+CoolGuySergey@users.noreply.github.com>
2024-05-17 22:23:23 +0800

    working modal and dialog buton

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ let songRepository = (function () {
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
     // apiUrl = 'https://api.lyrics.ovh/v1/Coldplay'
 
-    //--- loading message for loadSongsFromAPI() & loadDetailsFromAPI() ----------//
+    //----- loading message for loadSongsFromAPI() & loadDetailsFromAPI() --------//
     
     function showLoadingMessage() {
 	
@@ -186,5 +186,143 @@ songRepository.loadSongsFromAPI().then(function() {
     songRepository.getAllSongs().forEach(function(pokemon){
 	// for each song in songList, a button is created
 	songRepository.showSongButton(pokemon);
+    });
+});
+
+/* 2. Modals */
+
+let songModalDisplay = (function () {
+
+    let modalContainer = document.querySelector('.modal-container');
+
+    function showModal(SongTitle, SongText) {
+	modalContainer.classList.add('is-visible');
+
+	// Clear all existing html in container
+	// note the .innerHTML call
+	// modalContainer is the big shaded area when modal is open
+	modalContainer.innerHTML = '';
+
+	// Create modal-content div
+	// modalContent is the small window that the actual modal
+	let modalContent = document.createElement('div');
+	modalContent.classList.add('modal-content');
+	modalContainer.appendChild(modalContent);
+	
+	// Add other modal elements as children to modalContent
+	
+	// 1 of 3: close button
+	let modalCloseButton = document.createElement('button');
+	modalCloseButton.classList.add('modal-close-button');
+	modalCloseButton.innerText = 'Close';
+	//Event listener for exiting modal
+	modalCloseButton.addEventListener('click', hideModal);
+	modalContent.appendChild(modalCloseButton);
+
+	// 2 of 3: title
+	let modalTitle = document.createElement('h1');
+	modalTitle.innerText = SongTitle;
+	modalContent.appendChild(modalTitle);
+
+	// 3 of 3: text
+	let modalText = document.createElement('p');
+	modalText.innerText = SongText;
+	modalContent.appendChild(modalText);
+    }
+
+    let takePromiseOutOfLimbo;
+    // when the confirm button calls hideModal this will be set to false
+    // whenever hideModal is called any other way (by pressing the cancel button
+    // by clicking event, by esc keydown event, by the close button
+    // this will be true, and the promise will be rejeced
+    
+    function hideModal() {
+	modalContainer.classList.remove('is-visible');
+	
+	// the promise taken out of limbo and rejected
+	if (takePromiseOutOfLimbo) {
+	    takePromiseOutOfLimbo(); // this var was the reject function of the promise
+	    takePromiseOutOfLimbo = null;
+	}
+    }
+
+    // also call hideModal if ESC is pressed when modal is visible
+    window.addEventListener('keydown', (event) => {
+	if (event.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+	    hideModal();
+	}
+    });
+    
+    // also call hideModal if user clicks outside of modal while modal open
+    modalContainer.addEventListener('click', (event) => {
+	let target = event.target;
+	// console.log(target);
+	if (target === modalContainer) {
+	    hideModal();
+	}
+    });
+
+    function showDialog(header, message) {
+
+	// Borrows showModal's container html div
+	// as well as its functions to add title, message, and close button
+	// as well as all 3 closing methods since keydown and click event listeners were added to .modal-container
+	showModal(header, message);
+
+	let dialogContainer = document.querySelector('.modal-container');
+
+	// Add confirm and cancel button
+	let dialogContent = modalContainer.querySelector('.modal-content');
+
+	let confirmButton = document.createElement('button');
+	confirmButton.classList.add('dialog-confirm');
+	confirmButton.innerText = 'Confirm';
+	dialogContent.appendChild(confirmButton);
+
+	let cancelButton = document.createElement('button');
+	cancelButton.classList.add('modal-cancel');
+	cancelButton.innerText = 'Cancel';
+	dialogContent.appendChild(cancelButton);
+
+	// showDialog returns a promise that resolves when confirmed
+	// Rejects when the user exits the dialog or clicks "Cancel"
+	return new Promise((resolve, reject) => {
+	    
+	    cancelButton.addEventListener('click', hideModal); //hideModal will do the rejecting of the promise
+
+	    confirmButton.addEventListener('click', () => {
+		takePromiseOutOfLimbo = null; // Make sure to reset this from previous runs
+		hideModal();
+		resolve();
+	    });
+
+	    // If user closes the modal without confirming or canceling
+	    // i.e. exit with an ESC keydown or close-button click
+	    // the promise need to be taken out of limbo and rejected
+	    // The reject function of the promise is saved in  variable
+	    takePromiseOutOfLimbo = reject;
+	});
+    }
+
+    return {
+	showModal : showModal,
+	showDialog : showDialog,
+
+	// not exposed: hideModal()
+    };
+})();
+
+//Event-listener for showing modal
+
+document.querySelector('.show-modal-button').addEventListener('click', () => {
+    songModalDisplay.showModal('Bejeweled', 'Putting someone first only works when you\'re in their top five.');
+});
+
+document.querySelector('.show-dialog-button').addEventListener('click', () => {
+    //showDialog returns a promise
+    songModalDisplay.showDialog('Bejeweled', 'Putting someone first only works when you\'re in their top five.').then(function() {
+	alert('confirmed');
+    }, () => {
+	alert('not confirmed');
     });
 });
